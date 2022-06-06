0e68b1c6d5367ed1b7f0f87b938d1c73b516b3de
Lisa Chan <lchan@mozilla.com>
2022-06-06 08:56:02 -0400

    refactor(modal): create React component that combines payments/DialogMessage and settings/Modal

--- packages/fxa-react/components/Modal/index.tsx
+++ packages/fxa-react/components/Modal/index.tsx
@@ -3,12 +3,14 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 import React, { ReactNode } from 'react';
-import { useChangeFocusEffect, useClickOutsideEffect, useEscKeydownEffect } from 'fxa-react/lib/hooks';
+import { useClickOutsideEffect, useEscKeydownEffect, useChangeFocusEffect } from 'fxa-react/lib/hooks';
 import classNames from 'classnames';
 import Portal from 'fxa-react/components/Portal';
 import { ReactComponent as CloseIcon } from 'fxa-react/images/close.svg';
 import { Link, useLocation } from '@reach/router';
 import { Localized, useLocalization } from '@fluent/react';
+
+import './index.scss';
 
 type ModalProps = {
   className?: string;
@@ -17,21 +19,24 @@ type ModalProps = {
   children: ReactNode;
   hasButtons?: boolean;
   hasCancelButton?: boolean;
-  headerId: string;
-  descId: string;
+  headerId?: string;
+  descId?: string;
   route?: string;
   confirmText?: string;
   confirmBtnClassName?: string;
   'data-testid'?: string;
 };
 
+/* istanbul ignore next - not worth testing this function */
+const noop = () => {};
+
 export const Modal = ({
-  className = '',
+  className,
   onDismiss,
   onConfirm,
   children,
-  hasButtons = true,
-  hasCancelButton = true,
+  hasButtons,
+  hasCancelButton,
   headerId,
   descId,
   route,
@@ -39,11 +44,13 @@ export const Modal = ({
   confirmBtnClassName = 'cta-primary',
   'data-testid': testid = 'modal',
 }: ModalProps) => {
-  const modalInsideRef = useClickOutsideEffect<HTMLDivElement>(onDismiss);
+  const modalInsideRef = useClickOutsideEffect<HTMLDivElement>(onDismiss || noop);
   const tabFenceRef = useChangeFocusEffect();
-  const location = useLocation();
-  const { l10n } = useLocalization();
-  useEscKeydownEffect(onDismiss);
+  let location;
+  if (route) {
+    location = useLocation();
+  }
+  useEscKeydownEffect(onDismiss || noop);
 
   return (
     <Portal id="modal" {...{ headerId, descId }}>
@@ -54,7 +61,7 @@ export const Modal = ({
         <div
           data-testid="modal-content-container"
           className={classNames(
-            'max-w-md bg-white mx-auto rounded-xl',
+            'max-w-md bg-white mx-auto rounded-xl mobileLandscape:max-w-sm mobileLandscape:w-full',
             className
           )}
           ref={modalInsideRef}
@@ -65,33 +72,43 @@ export const Modal = ({
             data-testid="modal-tab-fence"
             className="outline-none"
           />
-          <div className="flex justify-end pr-2 pt-2">
-            <button
-              data-testid="modal-dismiss"
-              onClick={(event) => onDismiss()}
-              title={l10n.getString('modal-close-title', null, 'Close')}
-            >
-              <CloseIcon className="w-2 h-2 m-3" role="img" />
-            </button>
-          </div>
+
+          {onDismiss &&
+            <Localized id="close-aria">
+              <div className="flex justify-end pr-2 pt-2">
+                <button
+                  aria-label="Close modal"
+                  data-testid="modal-dismiss"
+                  onClick={() => onDismiss()}
+                >
+                  <CloseIcon
+                    aria-hidden="true"
+                    className="w-4 h-4 m-3"
+                    focusable="false"
+                    role="img"
+                  />
+                </button>
+              </div>
+            </Localized>
+          }
 
           <div className="px-6 tablet:px-10 pb-10">
-            <div>{children}</div>
+            <div className="text-center">{children}</div>
             {hasButtons && (
               <div className="flex justify-center mx-auto mt-6 max-w-64">
-                {hasCancelButton && (
+                {hasCancelButton && onDismiss && (
                   <Localized id="modal-cancel-button">
                     <button
                       className="cta-neutral mx-2 flex-1"
                       data-testid="modal-cancel"
-                      onClick={(event) => onDismiss()}
+                      onClick={() => onDismiss()}
                     >
                       Cancel
                     </button>
                   </Localized>
                 )}
 
-                {route && (
+                {route && location && (
                   <Link
                     className={classNames('mx-2 flex-1', confirmBtnClassName)}
                     data-testid="modal-confirm"
@@ -105,7 +122,7 @@ export const Modal = ({
                   <button
                     className={classNames('mx-2 flex-1', confirmBtnClassName)}
                     data-testid="modal-confirm"
-                    onClick={(event) => onConfirm()}
+                    onClick={() => onConfirm()}
                   >
                     {confirmText}
                   </button>
